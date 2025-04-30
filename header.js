c691e9518383c8375a0fc5c2ab3a20b7611dbdfa
Ogunniyi Daniel Ifeoluwa <ogunniyidanif18@gmail.com>
2025-04-30 03:03:59 +0100

    Update header.js

--- header.js
+++ header.js
@@ -236,7 +236,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 .nav-menu li a {
             text-decoration: none;
-            font-size: 20px;
+            font-size: 16px;
             color: black !important;
         }  
 
